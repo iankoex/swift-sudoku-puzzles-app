77a971b05cbb682d579eac2882c98315dc1800d1
iankoex <hello@iankoex.com>
2025-03-09 23:04:44 +0300

    Initial Commit

---
+++ Sudoku PuzzlesTests/Sudoku_PuzzlesTests.swift
@@ -0,0 +1,16 @@
+//
+//  Sudoku_PuzzlesTests.swift
+//  Sudoku PuzzlesTests
+//
+//  Created by ian on 09/03/2025.
+//
+
+import Testing
+
+struct Sudoku_PuzzlesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ Sudoku PuzzlesUITests/Sudoku_PuzzlesUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  Sudoku_PuzzlesUITestsLaunchTests.swift
+//  Sudoku PuzzlesUITests
+//
+//  Created by ian on 09/03/2025.
+//
+
+import XCTest
+
+final class Sudoku_PuzzlesUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
